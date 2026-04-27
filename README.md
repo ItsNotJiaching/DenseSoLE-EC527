dec176b2d499d592b6fc7b35e6ce36e6adbda21a
ItsNotJiaching <jiaxingw@bu.edu>
2026-04-27 01:58:33 -0400

    added different main files for testing, updated README

--- README.md
+++ README.md
@@ -1,21 +1,50 @@
-# DenseSoLE-EC527
+# Dense Systems of Linear Equations
+### EC527 Final Project, Spring 2026
 ### Jiaxing Wang, Alvin Yan, Owen Jiang
 
 ### Summary
-This project explores the implementation and optimization of dense systems of linear equations.
+This project explores the implementation and optimization of dense systems of linear equations. We start with a naive implementation of solving Ax=b through LU decomposition and forward and backward passes. Then, we optimize LU decomposition through blocking, vectorization, and parallelization through OpenMP and CUDA.
+
+### Implementations
+#### Serial
+1. Serial Naive
+2. Serial Blocked, version 1
+3. Serial Blocked, version 2
+#### Vectorized
+4. AVX
+#### OpenMP
+5. OpenMP Naive
+6. OpenMP Alternate Access Pattern
+7. OpenMP Attempts at Optimization (not good)
+#### CUDA
+8. CUDA Single-Kernel
+9. CUDA Two Kernels, Global Memory
+10. CUDA Two Kernels, Local Memory
+#### The Benchmark
+11. cuBLAS (Using cuBLAS library)
+
+To implement in the future: CUDA with Shared Memory and Blocking
 
 ### Compilation Notes
-To compile:
-
+To compile all tests:
 nvcc -O1 -Xcompiler "-fopenmp,-mavx,-mfma" main.cu sole_serial.cpp sole_multi.cpp sole_gpu.cu -o test_sole -lcusolver -lcublas -lm 
 
 To Run:
 OMP_NUM_THREADS=8 ./test_sole
 
+Just for blocking:
+gcc -O1 -mavx -mfma main_block.cpp sole_serial.cpp -o test_block
+./test_block
+
+Just for OpenMP:
+gcc -O1 -fopenmp main_omp.cpp sole_multi.cpp -o test_openmp
+OMP_NUM_THREADS=8 ./test_openmp
+(Change above with however many threads you intend to use)
+
+### Other Notes
 To request a GPU on the SCC:
 
 qrsh -l gpus=1 -l gpu_type=P100 -P ec527
 
 This will request a 6.0 compute GPU.
-
 Make sure to do "module load cuda" to get access to the nvcc compiler.
---
+++ main_omp.cpp
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#include <cstring>
+#include "sole.cuh"
+
+void init_matrix(data_t *mat, int len);
+void init_vector(data_t *mat, int len);
+double verify(data_t* arrA, data_t* arrX, data_t* arrB, int n);
+
+
+// Let's do multiples of 8 for now
+#define A   32  /* coefficient of x^2 */
+#define B   32  /* coefficient of x */
+#define C   32  /* constant term */
+
+#define NUM_TESTS 12   /* Number of different sizes to test */
+
+#define OPTIONS 1
+#define TOLERANCE 1e-4
+
+/* -=-=-=-=- Time measurement by clock_gettime() -=-=-=-=- */
+double interval(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  temp.tv_sec = end.tv_sec - start.tv_sec;
+  temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+  if (temp.tv_nsec < 0) {
+    temp.tv_sec = temp.tv_sec - 1;
+    temp.tv_nsec = temp.tv_nsec + 1000000000;
+  }
+  return (((double)temp.tv_sec) + ((double)temp.tv_nsec)*1.0e-9);
+}
+
+/* This routine "wastes" a little time to make sure the machine gets
+   out of power-saving mode and switches to normal speed. */
+double wakeup_delay()
+{
+  double meas = 0; int i, j;
+  struct timespec time_start, time_stop;
+  double quasi_random = 0;
+  clock_gettime(CLOCK_MONOTONIC, &time_start);
+  j = 100;
+  while (meas < 1.0) {
+    for (i=1; i<j; i++) {
+      /* This iterative calculation uses a chaotic map function, specifically
+         the complex quadratic map (as in Julia and Mandelbrot sets), which is
+         unpredictable enough to prevent compiler optimisation. */
+      quasi_random = quasi_random*quasi_random - 1.923432;
+    }
+    clock_gettime(CLOCK_MONOTONIC, &time_stop);
+    meas = interval(time_start, time_stop);
+    j *= 2; /* Twice as much delay next time, until we've taken 1 second */
+  }
+  return quasi_random;
+}
+
+/**
+ * Main Function. Only testing square matrices with array sizes multiple of 32.
+ */
+int main() {
+  // Initializations for time recording
+  struct timespec time_start, time_stop;
+  double time_stamp[OPTIONS][NUM_TESTS];
+  double error[OPTIONS][NUM_TESTS];
+  double wakeup_answer;
+  wakeup_answer = wakeup_delay();
+
+  // Initializations for determining array length
+  long int x, n, arr_len;
+  x = NUM_TESTS-1;
+  arr_len = A*x*x + B*x + C;
+
+  // Initalize data arrays to largest possible (to length arr_len)
+  data_t* arrA = (data_t *) calloc(arr_len * arr_len, sizeof(data_t));
+  data_t* arrB = (data_t *) calloc(arr_len, sizeof(data_t));
+  init_matrix(arrA, arr_len);
+  init_vector(arrB, arr_len);
+
+  // Terminal Output
+  printf("Running Dense System of Linear Equations (SoLE) Test ");
+  printf("for %d different matrix sizes from %d to %d\n\n", NUM_TESTS, C, arr_len);
+  detect_threads_setting();
+  printf("Can declare max %d threads on this processor\n", omp_get_num_procs());
+  printf("This may take a while!\n\n");
+
+  int OPTION = 0;
+  // OpenMP - Compile and run using directives to change number of threads
+  for (x=0; x<NUM_TESTS && (n = A*x*x + B*x + C, n<=arr_len); x++) {
+    //copy originals
+    data_t* arrA_copy = (data_t*) calloc(arr_len * arr_len, sizeof(data_t)); // copy for error check
+    data_t* arrB_copy = (data_t*) calloc(arr_len, sizeof(data_t)); // copy for error check
+    memcpy(arrA_copy, arrA, n*n*sizeof(data_t));
+    memcpy(arrB_copy, arrB, n*sizeof(data_t));
+    data_t* arrX = (data_t *) calloc(arr_len, sizeof(data_t));
+
+    printf(" Option %d, iter %ld, size %ld\n", OPTION, x, n);
+    clock_gettime(CLOCK_MONOTONIC, &time_start);
+    sole_omp_naive(arrA_copy, arrX, arrB_copy, n);
+    clock_gettime(CLOCK_MONOTONIC, &time_stop);
+    time_stamp[OPTION][x] = interval(time_start, time_stop);
+    error[OPTION][x] = verify(arrA, arrX, arrB,n);
+  }
+
+  printf("row_len, "
+        "omp, omp_err\n");
+
+  int i, j;
+  for (i = 0; i < NUM_TESTS; i++) {
+    printf("%ld, ", A*i*i + B*i + C);
+    for (j = 0; j < OPTIONS; j++) {
+      if (j != 0) {
+        printf(", ");
+      }
+      printf("%.1f, %.2f", (double) (1e9 * time_stamp[j][i]), error[j][i]);
+    }
+    printf("\n");
+  }
+  printf("Time units: ns\n");
+
+  printf("Wakeup delay computed: %g \n", wakeup_answer);
+  return 0;
+}
+
+void init_matrix(data_t *mat, int len) {
+  int i;
+  int max_num = 100; // changes this to initialize with higher numbers
+  // float randNum;
+  // srand(seed);
+
+  for (i = 0; i < len*len; i++) {
+    // randNum = (float) rand() / RAND_MAX; // let randNum be a random integer
+    // mat[i] = randNum;
+    mat[i] = data_t(rand() % (max_num - 1));
+  }
+}
+
+void init_vector(data_t *vec, int len) {
+  int i;
+  int max_num = 100; // changes this to initialize with higher numbers
+
+  for (i = 0; i < len; i++)
+    vec[i] = data_t(rand() % (max_num-1));
+}
+
+void print_array(data_t* v, int arr_len) {
+  for (int i=0; i < arr_len; i++) {
+    for (int j=0; j < arr_len; j++) {
+      printf("%.3f ", v[i*arr_len + j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+/**
+ * Computes the total amount of error that is ABOVE tolerance
+ * @returns The total amount of error that is ABOVE tolerance
+ * @authors Owen Jiang, Jiaxing Wang
+ */
+double verify(data_t* arrA, data_t* arrX, data_t* arrB, int n) {
+  double err = 0.0;
+  for (int i = 0; i < n; i++) {
+    double ax_i = 0.0;
+    for (int j = 0; j < n; j++)
+      ax_i += (double)arrA[i*n + j] * (double)arrX[j];
+    double diff = fabs(ax_i - (double)arrB[i]);
+    if (diff > TOLERANCE) err += diff;
+  }
+  return err;
+}
+
+/**
+ * Computes max |Ax - b| using the original A and b. 
+ * Expect some level of error when dealing with floating types,
+ * but it should be consistent across implementations.
+ * @returns The maximum error |Ax - b| of the data_t data type compared to double.
+ * @author Owen Jiang
+ */
+/* Original Version */
+// double verify(data_t* arrA, data_t* arrX, data_t* arrB, int n) {
+//   double max_err = 0.0;
+//   for (int i = 0; i < n; i++) {
+//     double ax_i = 0.0;
+//     for (int j = 0; j < n; j++)
+//       ax_i += (double)arrA[i*n + j] * (double)arrX[j];
+//     double err = fabs(ax_i - (double)arrB[i]);
+//     if (err > max_err) max_err = err;
+//   }
+//   return max_err;
+// }
